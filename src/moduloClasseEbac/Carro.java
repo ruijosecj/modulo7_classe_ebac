0f76d6ee2988ddd45cb36aea6b909b6e5affaa7f
ruijosecj <ruijosecj@gmail.com>
2023-07-22 19:46:04 -0300

    adicionando javadoc e comentários

--- src/moduloClasseEbac/Carro.java
+++ src/moduloClasseEbac/Carro.java
@@ -1,13 +1,23 @@
+/**
+ * @author ruijose
+ * 
+ * @
+ */
 package moduloClasseEbac;
 
 public class Carro {
-
+	
+	//Atributos
 	private String marca;
 	private String cor;
 	private String placa;
 	private Integer velocidadeAtual;
 	private Boolean chaveVirada;
-
+	
+	//Construtor padrão
+	public Carro() {}
+	
+	//Construtor com argumentos
 	public Carro(String marca, String cor, String placa) {
 		this.marca = marca;
 		this.cor = cor;
@@ -16,7 +26,7 @@ public class Carro {
 		this.chaveVirada = false;
 	}
 	
-
+	//metodos getter e setter
 	public String getMarca() {
 		return marca;
 	}
@@ -59,18 +69,20 @@ public class Carro {
 	public void setChaveVirada(Boolean chaveVirada) {
 		this.chaveVirada = chaveVirada;
 	}
-
+	
+	//metodo para ligar veiculo
 	public void ligar() {
 		chaveVirada = true;
 	}
-
+	
+	//metodo para acelerar veiculo
 	public void acelerar(Integer velocidade) {
 		if (!chaveVirada)
 			return;
 		this.velocidadeAtual = velocidade;
 	}
 
-
+	//metodo para apresentar as informações do veiculo
 	@Override
 	public String toString() {
 		return "Carro [marca=" + marca + ", cor=" + cor + ", placa=" + placa + ", velocidadeAtual=" + velocidadeAtual
